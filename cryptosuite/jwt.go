662e5ec4695e317a33665aed5b75d5edfbe59846
Gabe Cohen <gcohen@squareup.com>
2022-03-11 12:44:32 -0800

    refactor

--- cryptosuite/jwt.go
+++ cryptosuite/jwt.go
@@ -24,8 +24,8 @@ func (s *JSONWebKeySigner) SignVerifiableCredentialJWT(cred vc.VerifiableCredent
 	if cred.IsEmpty() {
 		return nil, errors.New("credential cannot be empty")
 	}
-	t := jwt.New()
 
+	t := jwt.New()
 	if cred.ExpirationDate != "" {
 		var expirationDate = cred.ExpirationDate
 		if unixTime, err := rfc3339ToUnix(cred.ExpirationDate); err == nil {
@@ -35,22 +35,28 @@ func (s *JSONWebKeySigner) SignVerifiableCredentialJWT(cred vc.VerifiableCredent
 			return nil, errors.Wrap(err, "could not set exp value")
 		}
 	}
+
 	if err := t.Set(jwt.IssuerKey, cred.Issuer); err != nil {
 		return nil, errors.Wrap(err, "could not set exp value")
 	}
+
 	var issuanceDate = cred.IssuanceDate
 	if unixTime, err := rfc3339ToUnix(cred.IssuanceDate); err == nil {
 		issuanceDate = string(unixTime)
 	}
+
 	if err := t.Set(jwt.NotBeforeKey, issuanceDate); err != nil {
 		return nil, errors.Wrap(err, "could not set nbf value")
 	}
+
 	if err := t.Set(jwt.JwtIDKey, cred.ID); err != nil {
 		return nil, errors.Wrap(err, "could not set jti value")
 	}
+
 	if err := t.Set(jwt.SubjectKey, cred.CredentialSubject.GetID()); err != nil {
 		return nil, errors.Wrap(err, "could not set subject value")
 	}
+
 	credBytes, err := util.PrettyJSON(cred)
 	if err != nil {
 		return nil, errors.New("could not marshal cred to JSON")
@@ -59,18 +65,17 @@ func (s *JSONWebKeySigner) SignVerifiableCredentialJWT(cred vc.VerifiableCredent
 	if err := t.Set(VCJWTProperty, credJSON); err != nil {
 		return nil, errors.New("could not set vc value")
 	}
+
 	return jwt.Sign(t, jwa.SignatureAlgorithm(s.GetSigningAlgorithm()), s.Key)
 }
 
 // VerifyVerifiableCredentialJWT verifies the signature validity on the token.
+// After signature validation, the JWT is decoded according to the specification.
+// https://www.w3.org/TR/vc-data-model/#jwt-decoding
+// If there are any issues during decoding, an error is returned. As a result, a successfully
+// decoded VerifiableCredential object is returned.
 func (v *JSONWebKeyVerifier) VerifyVerifiableCredentialJWT(token string) error {
-	_, err := jwt.Parse([]byte(token), jwt.WithVerify(jwa.SignatureAlgorithm(v.Algorithm()), v.Key))
-	return err
-}
-
-// VerifiableCredentialFromJWT follows the decoding steps from the spec https://www.w3.org/TR/vc-data-model/#jwt-decoding
-func VerifiableCredentialFromJWT(token string) (*vc.VerifiableCredential, error) {
-	return nil, nil
+	return v.VerifyJWT(token)
 }
 
 // SignVerifiablePresentationJWT is prepared according to https://www.w3.org/TR/vc-data-model/#jwt-encoding
@@ -78,13 +83,16 @@ func (s *JSONWebKeySigner) SignVerifiablePresentationJWT(pres vc.VerifiablePrese
 	if pres.IsEmpty() {
 		return nil, errors.New("presentation cannot be empty")
 	}
+
 	t := jwt.New()
 	if err := t.Set(jwt.JwtIDKey, pres.ID); err != nil {
 		return nil, errors.Wrap(err, "could not set jti value")
 	}
+
 	if err := t.Set(jwt.SubjectKey, pres.Holder); err != nil {
 		return nil, errors.New("could not set subject value")
 	}
+
 	presBytes, err := util.PrettyJSON(pres)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not marshal cred to JSON")
@@ -93,21 +101,27 @@ func (s *JSONWebKeySigner) SignVerifiablePresentationJWT(pres vc.VerifiablePrese
 	if err := t.Set(VPJWTProperty, presJSON); err != nil {
 		return nil, errors.Wrap(err, "could not set vp value")
 	}
+
 	if err := t.Set(NonceProperty, uuid.New().String()); err != nil {
 		return nil, errors.Wrap(err, "could not set nonce value")
 	}
+
 	return jwt.Sign(t, jwa.SignatureAlgorithm(s.GetSigningAlgorithm()), s.Key)
 }
 
 // VerifyVerifiablePresentationJWT verifies the signature validity on the token.
+// After signature validation, the JWT is decoded according to the specification.
+// https://www.w3.org/TR/vc-data-model/#jwt-decoding
+// If there are any issues during decoding, an error is returned. As a result, a successfully
+// decoded VerifiablePresentation object is returned.
 func (v *JSONWebKeyVerifier) VerifyVerifiablePresentationJWT(token string) error {
-	_, err := jwt.Parse([]byte(token), jwt.WithVerify(jwa.SignatureAlgorithm(v.Algorithm()), v.Key))
-	return err
+	return v.VerifyJWT(token)
 }
 
-// VerifiablePresentationFromJWT follows the decoding steps from the spec https://www.w3.org/TR/vc-data-model/#jwt-decoding
-func VerifiablePresentationFromJWT(token string) (*vc.VerifiablePresentation, error) {
-	return nil, nil
+// VerifyJWT parses a token given the verifier's known algorithm and key, and returns an error, which is nil upon success
+func (v *JSONWebKeyVerifier) VerifyJWT(token string) error {
+	_, err := jwt.Parse([]byte(token), jwt.WithVerify(jwa.SignatureAlgorithm(v.Algorithm()), v.Key))
+	return err
 }
 
 // according to the spec the JWT timestamp must be a `NumericDate` property, which is a JSON Unix timestamp value.
