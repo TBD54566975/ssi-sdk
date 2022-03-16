2b6c933274e6d3ac59161274062f4b6fd3d9060c
Gabe Cohen <gcohen@squareup.com>
2022-03-15 20:53:17 -0700

    basic data model in place

--- manifest/model.go
+++ manifest/model.go
@@ -14,7 +14,7 @@ type CredentialManifest struct {
 type Issuer struct {
 	ID   string `json:"id" validate:"required"`
 	Name string `json:"name,omitempty"`
-	// an object or URI as defined by the  DIFEntity Styles specification
+	// an object or URI as defined by the DIF Entity Styles specification
 	// https://identity.foundation/wallet-rendering/#entity-styles
 	Styles interface{} `json:"styles,omitempty"`
 }
@@ -37,3 +37,28 @@ type OutputDescriptor struct {
 func (cm *CredentialManifest) IsValid() error {
 	return util.NewValidator().Struct(cm)
 }
+
+// CredentialApplication https://identity.foundation/credential-manifest/#credential-application
+type CredentialApplication struct {
+	Application Application `json:"credential_application" validate:"required"`
+	// Must be present if the corresponding manifest contains a presentation_definition
+	// TODO(gabe) point to impl after https://github.com/TBD54566975/did-sdk/issues/18
+	PresentationSubmission interface{} `json:"presentation_submission,omitempty"`
+}
+
+type Application struct {
+	ID         string `json:"id" validate:"required"`
+	ManifestID string `json:"manifest_id" validate:"required"`
+	// TODO(gabe) unify format with the pres def impl in https://github.com/TBD54566975/did-sdk/issues/18
+	Format interface{} `json:"format" validate:"required"`
+}
+
+// CredentialFulfillment https://identity.foundation/credential-manifest/#credential-fulfillment
+type CredentialFulfillment struct {
+	ID         string `json:"id" validate:"required"`
+	ManifestID string `json:"manifest_id" validate:"required"`
+	// TODO(gabe) unify with https://identity.foundation/presentation-exchange/#presentation-submission
+	DescriptorMap interface{} `json:"descriptor_map" validate:"required"`
+}
+
+// TODO(gabe) support multiple embed targets https://identity.foundation/credential-manifest/#embed-targets-2
