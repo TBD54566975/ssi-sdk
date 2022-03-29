decd7aa9f8694eb77907d771c20ab26d395be0dc
Gabe Cohen <gcohen@squareup.com>
2022-03-29 13:35:08 -0700

    verification logic

--- credential/exchange/verification.go
+++ credential/exchange/verification.go
@@ -6,6 +6,7 @@ import (
 	"github.com/TBD54566975/did-sdk/credential/signing"
 	"github.com/TBD54566975/did-sdk/cryptosuite"
 	"github.com/TBD54566975/did-sdk/util"
+	"github.com/oliveagle/jsonpath"
 	"github.com/pkg/errors"
 	"strings"
 )
@@ -53,7 +54,8 @@ func VerifyPresentationSubmissionVP(def PresentationDefinition, vp credential.Ve
 		return errors.Wrap(err, "invalid presentation submission in provided verifiable presentation")
 	}
 	if submission.DefinitionID != def.ID {
-		return fmt.Errorf("mismatched between presentation definition ID<%s> and submission's definition ID<%s>", def.ID, submission.DefinitionID)
+		return fmt.Errorf("mismatched between presentation definition ID<%s> and submission's definition ID<%s>",
+			def.ID, submission.DefinitionID)
 	}
 
 	// index submission descriptors by id of the input descriptor
@@ -62,9 +64,12 @@ func VerifyPresentationSubmissionVP(def PresentationDefinition, vp credential.Ve
 		submissionDescriptorLookup[d.ID] = d
 	}
 
-	// turn the vp into JSON so we can use the paths from the submission descriptor
+	// turn the vp into JSON so we can use the paths from the submission descriptor to resolve each claim
+	vpJSON, err := util.ToJSONMap(vp)
+	if err != nil {
+		return errors.Wrap(err, "could not turn VP into JSON representation")
+	}
 
-	//credentials := vp.VerifiableCredential
 	// validate each input descriptor is fulfilled
 	for _, inputDescriptor := range def.InputDescriptors {
 		submissionDescriptor, ok := submissionDescriptorLookup[inputDescriptor.ID]
@@ -72,14 +77,55 @@ func VerifyPresentationSubmissionVP(def PresentationDefinition, vp credential.Ve
 			return fmt.Errorf("unfulfilled input descriptor<%s>; submission not valid", inputDescriptor.ID)
 		}
 
-		// make sure the format is as expected
+		// if the format on the submitted claim does not match the input descriptor, we cannot process
 		if !util.Contains(submissionDescriptor.Format, inputDescriptor.Format.FormatValues()) {
-			return fmt.Errorf("unsupported format<%s> for input descriptor which supports: %s", submissionDescriptor.Format, strings.Join(inputDescriptor.Format.FormatValues(), ", "))
+			return fmt.Errorf("for input descriptor<%s>, the format of submission descriptor<%s> is not one"+
+				"  of the supported formats: %s", inputDescriptor.ID, submissionDescriptor.Format,
+				strings.Join(inputDescriptor.Format.FormatValues(), ", "))
 		}
 
-		// get cred from index
-		// apply path and see if it checks out!
-		//submissionDescriptor.Path
+		// TODO(gabe) support nested paths in presentation submissions
+		// https://github.com/TBD54566975/did-sdk/issues/73
+		if submissionDescriptor.PathNested != nil {
+			return fmt.Errorf("submission with nested paths not supported: %s", submissionDescriptor.ID)
+		}
+
+		// make sure the format is as expected
+		if !util.Contains(submissionDescriptor.Format, inputDescriptor.Format.FormatValues()) {
+			return fmt.Errorf("unsupported format<%s> for input descriptor which supports: %s",
+				submissionDescriptor.Format, strings.Join(inputDescriptor.Format.FormatValues(), ", "))
+		}
+
+		// resolve the claim from the JSON path expression in the submission descriptor
+		submittedClaim, err := jsonpath.JsonPathLookup(vpJSON, submissionDescriptor.Path)
+		if err != nil {
+			return errors.Wrapf(err, "could not resolve claim from submission descriptor<%s> with path: %s",
+				submissionDescriptor.ID, submissionDescriptor.Path)
+		}
+
+		// verify the submitted claim complies with the input descriptor
+
+		// if there are no constraints, we are done checking for validity
+		if inputDescriptor.Constraints == nil {
+			continue
+		}
+
+		// TODO(gabe) consider enforcing limited disclosure if present
+		// for each field we need to verify at least one path matches
+		for _, field := range inputDescriptor.Constraints.Fields {
+			if err := findMatchingPath(submittedClaim, field.Path); err != nil {
+				return errors.Wrapf(err, "input descriptor<%s> not fulfilled for field: %s", inputDescriptor.ID, field.ID)
+			}
+		}
 	}
 	return nil
+}
+
+func findMatchingPath(claim interface{}, paths []string) error {
+	for _, path := range paths {
+		if _, err := jsonpath.JsonPathLookup(claim, path); err == nil {
+			return nil
+		}
+	}
+	return errors.New("matching path for claim could not be found")
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/TBD54566975/did-sdk
 go 1.17
 
 require (
-	github.com/PaesslerAG/jsonpath v0.1.1
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1
 	github.com/go-playground/validator/v10 v10.10.0
 	github.com/gobuffalo/packr/v2 v2.8.3
@@ -15,6 +14,7 @@ require (
 	github.com/multiformats/go-multibase v0.0.3
 	github.com/multiformats/go-multicodec v0.4.0
 	github.com/multiformats/go-varint v0.0.6
+	github.com/oliveagle/jsonpath v0.0.0-20180606110733-2e52cf6e6852
 	github.com/piprate/json-gold v0.4.1
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.7.0
@@ -23,16 +23,11 @@ require (
 )
 
 require (
-	github.com/PaesslerAG/gval v1.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/gobuffalo/envy v1.7.0 // indirect
 	github.com/gobuffalo/logger v1.0.6 // indirect
 	github.com/gobuffalo/packd v1.0.1 // indirect
-	github.com/gobuffalo/packr v1.30.1 // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/karrick/godirwalk v1.16.1 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
@@ -45,16 +40,11 @@ require (
 	github.com/markbates/safe v1.0.1 // indirect
 	github.com/multiformats/go-base32 v0.0.3 // indirect
 	github.com/multiformats/go-base36 v0.1.0 // indirect
-	github.com/oliveagle/jsonpath v0.0.0-20180606110733-2e52cf6e6852 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
-	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
-	github.com/spf13/cobra v1.2.1 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.6 // indirect
