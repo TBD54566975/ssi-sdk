10a7c8c3e8dac5d183f6fde3df693b84b4c8619f
Gabe Cohen <gcohen@squareup.com>
2022-03-28 20:55:23 -0700

    structure and simple tests

--- credential/exchange/verification.go
+++ credential/exchange/verification.go
@@ -1,5 +1,40 @@
+//go:build jwx_es256k
+
 package exchange
 
-func VerifyPresentationSubmission() error {
+import (
+	"fmt"
+	"github.com/TBD54566975/did-sdk/credential"
+	"github.com/TBD54566975/did-sdk/credential/signing"
+	"github.com/TBD54566975/did-sdk/cryptosuite"
+	"github.com/pkg/errors"
+)
+
+// VerifyPresentationSubmission verifies a presentation submission for both signature validity and correctness
+// with the specification. It is assumed that the caller knows the submission embed target, and the corresponding
+// presentation definition, and has access to the public key of the signer.
+func VerifyPresentationSubmission(verifier cryptosuite.Verifier, et EmbedTarget, def PresentationDefinition, submission []byte) error {
+	if !IsSupportedEmbedTarget(et) {
+		return fmt.Errorf("unsupported presentation submission embed target type: %s", et)
+	}
+	switch et {
+	case JWTVPTarget:
+		jwkVerifier, ok := verifier.(*cryptosuite.JSONWebKeyVerifier)
+		if !ok {
+			return fmt.Errorf("verifier not valid for request type: %s", et)
+		}
+		vp, err := signing.VerifyVerifiablePresentationJWT(*jwkVerifier, string(submission))
+		if err != nil {
+			return errors.Wrap(err, "verification of the presentation submission failed")
+		}
+		return VerifyPresentationSubmissionVP(def, *vp)
+	default:
+		return fmt.Errorf("presentation submission embed target <%s> is not implemented", et)
+	}
+}
+
+// VerifyPresentationSubmissionVP verifies whether a verifiable presentation is a valid presentation submission
+// for a given presentation definition.
+func VerifyPresentationSubmissionVP(def PresentationDefinition, vp credential.VerifiablePresentation) error {
 	return nil
 }
---
+++ credential/exchange/verification_test.go
@@ -0,0 +1,101 @@
+//go:build jwx_es256k
+
+package exchange
+
+import (
+	"github.com/TBD54566975/did-sdk/credential/signing"
+	"github.com/TBD54566975/did-sdk/cryptosuite"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestVerifyPresentationSubmission(t *testing.T) {
+	t.Run("Unsupported embed target", func(tt *testing.T) {
+		verifier := cryptosuite.JSONWebKeyVerifier{}
+		err := VerifyPresentationSubmission(&verifier, "badEmbedTarget", PresentationDefinition{}, nil)
+		assert.Error(tt, err)
+		assert.Contains(tt, err.Error(), "unsupported presentation submission embed target type")
+	})
+
+	t.Run("Supported embed target, bad submission", func(tt *testing.T) {
+		jwk, err := cryptosuite.GenerateJSONWebKey2020(cryptosuite.OKP, cryptosuite.Ed25519)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, jwk)
+
+		verifier, err := cryptosuite.NewJSONWebKeyVerifier(jwk.ID, jwk.PublicKeyJWK)
+		assert.NoError(tt, err)
+
+		def := PresentationDefinition{
+			ID: "test-id",
+			InputDescriptors: []InputDescriptor{
+				{
+					ID: "id-1",
+					Constraints: &Constraints{
+						Fields: []Field{
+							{
+								Path:    []string{"$.vc.issuer", "$.issuer"},
+								ID:      "issuer-input-descriptor",
+								Purpose: "need to check the issuer",
+							},
+						},
+					},
+				},
+			},
+		}
+		err = VerifyPresentationSubmission(verifier, JWTVPTarget, def, nil)
+		assert.Error(tt, err)
+		assert.Contains(tt, err.Error(), "verification of the presentation submission failed")
+	})
+
+	t.Run("Supported embed target, valid submission", func(tt *testing.T) {
+
+	})
+}
+
+func TestVerifyPresentationSubmissionVP(t *testing.T) {
+	t.Run("Simple verification", func(tt *testing.T) {
+		jwk, err := cryptosuite.GenerateJSONWebKey2020(cryptosuite.OKP, cryptosuite.Ed25519)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, jwk)
+
+		signer, err := cryptosuite.NewJSONWebKeySigner(jwk.ID, jwk.PrivateKeyJWK, cryptosuite.AssertionMethod)
+		assert.NoError(t, err)
+
+		def := PresentationDefinition{
+			ID: "test-id",
+			InputDescriptors: []InputDescriptor{
+				{
+					ID: "id-1",
+					Constraints: &Constraints{
+						Fields: []Field{
+							{
+								Path:    []string{"$.vc.issuer", "$.issuer"},
+								ID:      "issuer-input-descriptor",
+								Purpose: "need to check the issuer",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		assert.NoError(tt, def.IsValid())
+		testVC := getTestVerifiableCredential()
+		presentationClaim := PresentationClaim{
+			Credential:                    &testVC,
+			LDPFormat:                     LDPVC.Ptr(),
+			SignatureAlgorithmOrProofType: string(cryptosuite.JSONWebSignature2020),
+		}
+		submissionBytes, err := BuildPresentationSubmission(signer, def, []PresentationClaim{presentationClaim}, JWTVPTarget)
+		assert.NoError(tt, err)
+		assert.NotEmpty(tt, submissionBytes)
+
+		verifier, err := cryptosuite.NewJSONWebKeyVerifier(jwk.ID, jwk.PublicKeyJWK)
+		assert.NoError(tt, err)
+		vp, err := signing.VerifyVerifiablePresentationJWT(*verifier, string(submissionBytes))
+		assert.NoError(tt, err)
+
+		assert.NoError(tt, vp.IsValid())
+		assert.Equal(tt, 1, len(vp.VerifiableCredential))
+	})
+}
