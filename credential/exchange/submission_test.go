248ff20705678bbe276ef4043c4fed59ce0a390e
Gabe Cohen <gcohen@squareup.com>
2022-03-28 20:21:17 -0700

    submission testing done

--- credential/exchange/submission_test.go
+++ credential/exchange/submission_test.go
@@ -4,6 +4,7 @@ package exchange
 
 import (
 	"github.com/TBD54566975/did-sdk/credential"
+	"github.com/TBD54566975/did-sdk/credential/signing"
 	"github.com/TBD54566975/did-sdk/crypto"
 	"github.com/TBD54566975/did-sdk/cryptosuite"
 	"github.com/goccy/go-json"
@@ -14,7 +15,63 @@ import (
 )
 
 func TestBuildPresentationSubmission(t *testing.T) {
+	t.Run("Unsupported embed target", func(tt *testing.T) {
+		jwk, err := cryptosuite.GenerateJSONWebKey2020(cryptosuite.OKP, cryptosuite.Ed25519)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, jwk)
 
+		signer, err := cryptosuite.NewJSONWebKeySigner(jwk.ID, jwk.PrivateKeyJWK, cryptosuite.AssertionMethod)
+		assert.NoError(t, err)
+		_, err = BuildPresentationSubmission(signer, PresentationDefinition{}, nil, "badEmbedTarget")
+		assert.Error(tt, err)
+		assert.Contains(tt, err.Error(), "unsupported presentation submission embed target type")
+	})
+
+	t.Run("Supported embed target", func(tt *testing.T) {
+		jwk, err := cryptosuite.GenerateJSONWebKey2020(cryptosuite.OKP, cryptosuite.Ed25519)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, jwk)
+
+		signer, err := cryptosuite.NewJSONWebKeySigner(jwk.ID, jwk.PrivateKeyJWK, cryptosuite.AssertionMethod)
+		assert.NoError(t, err)
+
+		def := PresentationDefinition{
+			ID: "test-id",
+			InputDescriptors: []InputDescriptor{
+				{
+					ID: "id-1",
+					Constraints: &Constraints{
+						Fields: []Field{
+							{
+								Path:    []string{"$.vc.issuer", "$.issuer"},
+								ID:      "issuer-input-descriptor",
+								Purpose: "need to check the issuer",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		assert.NoError(tt, def.IsValid())
+		testVC := getTestVerifiableCredential()
+		presentationClaim := PresentationClaim{
+			Credential:                    &testVC,
+			LDPFormat:                     LDPVC.Ptr(),
+			SignatureAlgorithmOrProofType: string(cryptosuite.JSONWebSignature2020),
+		}
+		submissionBytes, err := BuildPresentationSubmission(signer, def, []PresentationClaim{presentationClaim}, JWTVPTarget)
+		assert.NoError(tt, err)
+		assert.NotEmpty(tt, submissionBytes)
+
+		verifier, err := cryptosuite.NewJSONWebKeyVerifier(jwk.ID, jwk.PublicKeyJWK)
+		assert.NoError(tt, err)
+		vp, err := signing.VerifyVerifiablePresentationJWT(*verifier, string(submissionBytes))
+		assert.NoError(tt, err)
+
+		assert.NoError(tt, vp.IsValid())
+		assert.Equal(tt, 1, len(vp.VerifiableCredential))
+	})
 }
 
 func TestBuildPresentationSubmissionVP(t *testing.T) {
