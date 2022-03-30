09ca42a338c3e8ea8de018b0fc07f673c8a6209d
Gabe <gcohen@squareup.com>
2022-03-30 00:27:33 -0400

    Presentation Exchange: Submission Validation (#70)
    * structure and simple tests
    
    * temp
    
    * build tags
    
    * remove unnecessary build tags
    
    * verification logic
    
    * verification testS

---
+++ credential/exchange/verification_test.go
@@ -0,0 +1,249 @@
+package exchange
+
+import (
+	"github.com/TBD54566975/did-sdk/credential"
+	"github.com/TBD54566975/did-sdk/credential/signing"
+	"github.com/TBD54566975/did-sdk/cryptosuite"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestVerifyPresentationSubmission(t *testing.T) {
+	t.Run("Unsupported embed target", func(tt *testing.T) {
+		verifier := cryptosuite.JSONWebKeyVerifier{}
+		err := VerifyPresentationSubmission(&verifier, "badEmbedTarget", PresentationDefinition{}, nil)
+		assert.Error(tt, err)
+		assert.Contains(tt, err.Error(), "unsupported presentation submission embed target type")
+	})
+
+	t.Run("Supported embed target, bad submission", func(tt *testing.T) {
+		def := PresentationDefinition{
+			ID: "test-id",
+			InputDescriptors: []InputDescriptor{
+				{
+					ID: "id-1",
+					Constraints: &Constraints{
+						Fields: []Field{
+							{
+								Path:    []string{"$.vc.issuer", "$.issuer"},
+								ID:      "issuer-input-descriptor",
+								Purpose: "need to check the issuer",
+							},
+						},
+					},
+				},
+			},
+		}
+		_, verifier := getJWKSignerVerifier(tt)
+		err := VerifyPresentationSubmission(verifier, JWTVPTarget, def, nil)
+		assert.Error(tt, err)
+		assert.Contains(tt, err.Error(), "verification of the presentation submission failed")
+	})
+
+	t.Run("Supported embed target, valid submission", func(tt *testing.T) {
+		def := PresentationDefinition{
+			ID: "test-id",
+			InputDescriptors: []InputDescriptor{
+				{
+					ID: "id-1",
+					Constraints: &Constraints{
+						Fields: []Field{
+							{
+								Path:    []string{"$.vc.issuer", "$.issuer"},
+								ID:      "issuer-input-descriptor",
+								Purpose: "need to check the issuer",
+							},
+						},
+					},
+				},
+			},
+		}
+		assert.NoError(tt, def.IsValid())
+
+		signer, verifier := getJWKSignerVerifier(tt)
+		testVC := getTestVerifiableCredential()
+		presentationClaim := PresentationClaim{
+			Credential:                    &testVC,
+			LDPFormat:                     LDPVC.Ptr(),
+			SignatureAlgorithmOrProofType: string(cryptosuite.JSONWebSignature2020),
+		}
+		submissionBytes, err := BuildPresentationSubmission(signer, def, []PresentationClaim{presentationClaim}, JWTVPTarget)
+		assert.NoError(tt, err)
+		assert.NotEmpty(tt, submissionBytes)
+
+		err = VerifyPresentationSubmission(verifier, JWTVPTarget, def, submissionBytes)
+		assert.NoError(tt, err)
+	})
+}
+
+func TestVerifyPresentationSubmissionVP(t *testing.T) {
+	t.Run("Simple verification", func(tt *testing.T) {
+		def := PresentationDefinition{
+			ID: "test-id",
+			InputDescriptors: []InputDescriptor{
+				{
+					ID: "id-1",
+					Constraints: &Constraints{
+						Fields: []Field{
+							{
+								Path:    []string{"$.vc.issuer", "$.issuer"},
+								ID:      "issuer-input-descriptor",
+								Purpose: "need to check the issuer",
+							},
+						},
+					},
+				},
+			},
+		}
+		assert.NoError(tt, def.IsValid())
+
+		signer, _ := getJWKSignerVerifier(tt)
+		testVC := getTestVerifiableCredential()
+		presentationClaim := PresentationClaim{
+			Credential:                    &testVC,
+			LDPFormat:                     LDPVC.Ptr(),
+			SignatureAlgorithmOrProofType: string(cryptosuite.JSONWebSignature2020),
+		}
+		submissionBytes, err := BuildPresentationSubmission(signer, def, []PresentationClaim{presentationClaim}, JWTVPTarget)
+		assert.NoError(tt, err)
+		assert.NotEmpty(tt, submissionBytes)
+
+		verifiablePresentation, err := signing.ParseVerifiablePresentationFromJWT(string(submissionBytes))
+		assert.NoError(tt, err)
+
+		err = VerifyPresentationSubmissionVP(def, *verifiablePresentation)
+		assert.NoError(tt, err)
+	})
+
+	t.Run("Missing Claim in Submission", func(tt *testing.T) {
+		def := PresentationDefinition{
+			ID: "test-id",
+			InputDescriptors: []InputDescriptor{
+				{
+					ID: "id-1",
+					Constraints: &Constraints{
+						Fields: []Field{
+							{
+								Path:    []string{"$.vc.issuer", "$.issuer"},
+								ID:      "issuer-input-descriptor",
+								Purpose: "need to check the issuer",
+							},
+						},
+					},
+				},
+			},
+		}
+		assert.NoError(tt, def.IsValid())
+
+		presentation := credential.VerifiablePresentation{
+			Context: []string{"https://www.w3.org/2018/credentials/v1",
+				"https://identity.foundation/presentation-exchange/submission/v1"},
+			ID:   "55da1f5c-e2b3-443a-b687-0434712c5469",
+			Type: []string{"VerifiablePresentation", "PresentationSubmission"},
+			PresentationSubmission: PresentationSubmission{
+				ID:           "45da2588-3637-45b0-84f1-17e97945ac09",
+				DefinitionID: "test-id",
+				DescriptorMap: []SubmissionDescriptor{
+					{
+						Format: "ldp_vc",
+						ID:     "id-1",
+						Path:   "$.verifiableCredential[0]",
+					},
+				},
+			},
+		}
+
+		err := VerifyPresentationSubmissionVP(def, presentation)
+		assert.Error(tt, err)
+		assert.Contains(tt, err.Error(), "could not resolve claim from submission descriptor<id-1> with path: $.verifiableCredential[0]")
+	})
+
+	t.Run("Input Descriptor Not Fulfilled in Submission", func(tt *testing.T) {
+		def := PresentationDefinition{
+			ID: "test-id",
+			InputDescriptors: []InputDescriptor{
+				{
+					ID: "id-1",
+					Constraints: &Constraints{
+						Fields: []Field{
+							{
+								Path:    []string{"$.vc.issuer", "$.issuer"},
+								ID:      "issuer-input-descriptor",
+								Purpose: "need to check the issuer",
+							},
+						},
+					},
+				},
+			},
+		}
+		assert.NoError(tt, def.IsValid())
+
+		presentation := credential.VerifiablePresentation{
+			Context: []string{"https://www.w3.org/2018/credentials/v1",
+				"https://identity.foundation/presentation-exchange/submission/v1"},
+			ID:   "55da1f5c-e2b3-443a-b687-0434712c5469",
+			Type: []string{"VerifiablePresentation", "PresentationSubmission"},
+			PresentationSubmission: PresentationSubmission{
+				ID:           "45da2588-3637-45b0-84f1-17e97945ac09",
+				DefinitionID: "test-id",
+				DescriptorMap: []SubmissionDescriptor{
+					{
+						Format: "ldp_vc",
+						ID:     "id-2",
+						Path:   "$.verifiableCredential[0]",
+					},
+				},
+			},
+		}
+
+		err := VerifyPresentationSubmissionVP(def, presentation)
+		assert.Error(tt, err)
+		assert.Contains(tt, err.Error(), "unfulfilled input descriptor<id-1>; submission not valid")
+	})
+
+	t.Run("Input Descriptor with Bad Path", func(tt *testing.T) {
+		def := PresentationDefinition{
+			ID: "test-id",
+			InputDescriptors: []InputDescriptor{
+				{
+					ID: "id-1",
+					Constraints: &Constraints{
+						Fields: []Field{
+							{
+								Path:    []string{"$bad"},
+								ID:      "issuer-input-descriptor",
+								Purpose: "need to check the issuer",
+							},
+						},
+					},
+				},
+			},
+		}
+		assert.NoError(tt, def.IsValid())
+
+		presentation := credential.VerifiablePresentation{
+			Context: []string{"https://www.w3.org/2018/credentials/v1",
+				"https://identity.foundation/presentation-exchange/submission/v1"},
+			ID:   "55da1f5c-e2b3-443a-b687-0434712c5469",
+			Type: []string{"VerifiablePresentation", "PresentationSubmission"},
+			PresentationSubmission: PresentationSubmission{
+				ID:           "45da2588-3637-45b0-84f1-17e97945ac09",
+				DefinitionID: "test-id",
+				DescriptorMap: []SubmissionDescriptor{
+					{
+						Format: "ldp_vc",
+						ID:     "id-1",
+						Path:   "$.verifiableCredential[0]",
+					},
+				},
+			},
+			VerifiableCredential: []interface{}{
+				getTestVerifiableCredential(),
+			},
+		}
+
+		err := VerifyPresentationSubmissionVP(def, presentation)
+		assert.Error(tt, err)
+		assert.Contains(tt, err.Error(), "matching path for claim could not be found")
+	})
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/TBD54566975/did-sdk
 go 1.17
 
 require (
-	github.com/PaesslerAG/jsonpath v0.1.1
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1
 	github.com/go-playground/validator/v10 v10.10.0
 	github.com/gobuffalo/packr/v2 v2.8.3
@@ -15,6 +14,7 @@ require (
 	github.com/multiformats/go-multibase v0.0.3
 	github.com/multiformats/go-multicodec v0.4.0
 	github.com/multiformats/go-varint v0.0.6
+	github.com/oliveagle/jsonpath v0.0.0-20180606110733-2e52cf6e6852
 	github.com/piprate/json-gold v0.4.1
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.7.0
@@ -23,7 +23,6 @@ require (
 )
 
 require (
-	github.com/PaesslerAG/gval v1.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
@@ -45,7 +44,6 @@ require (
 	github.com/markbates/safe v1.0.1 // indirect
 	github.com/multiformats/go-base32 v0.0.3 // indirect
 	github.com/multiformats/go-base36 v0.1.0 // indirect
-	github.com/oliveagle/jsonpath v0.0.0-20180606110733-2e52cf6e6852 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
