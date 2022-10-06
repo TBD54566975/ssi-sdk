b44e39fd00d43b232118f4ee4fc377358925a8bc
gabe <gcohen@squareup.com>
2022-10-05 18:56:22 -0700

    cred verification

--- credential/verification/verification.go
+++ credential/verification/verification.go
@@ -8,32 +8,46 @@ import (
 	"github.com/pkg/errors"
 )
 
-// CredentialVerifier does three levels of verification on a credential:
-// 1. Makes sure the credential has a valid signature
-// 2. Makes sure the credential has is not expired
-// 3. Makes sure the credential complies with the VC Data Model
-// 4. If the credential has a schema, makes sure its data complies with the schema
-// LATER: Makes sure the credential has not been revoked, other checks.
-// Note: https://github.com/TBD54566975/ssi-sdk/issues/213
 type CredentialVerifier struct {
 	verifiers []Verifier
 }
 
 type Verifier struct {
-	Description string
-	VerifyFunc  Verify
+	ID         string
+	VerifyFunc Verify
 }
 
-type Verify func(credential.VerifiableCredential) error
+type (
+	// OptionKey uniquely represents an option to be used in a verifier
+	OptionKey string
+)
 
-// NewCredentialVerifier creates a new credential verifier
+// VerificationOption represents a single option that may be required for a verifier
+type VerificationOption struct {
+	ID     OptionKey
+	Option interface{}
+}
+
+// GetVerificationOption	returns a verification option given an ID
+func GetVerificationOption(opts []VerificationOption, id OptionKey) (interface{}, error) {
+	for _, opt := range opts {
+		if opt.ID == id {
+			return opt.Option, nil
+		}
+	}
+	return nil, errors.Errorf("option with id <%s> not found", id)
+}
+
+type Verify func(credential credential.VerifiableCredential, opts ...VerificationOption) error
+
+// NewCredentialVerifier creates a new credential verifier which executes in the order of the verifiers provided
 func NewCredentialVerifier(verifiers []Verifier) (*CredentialVerifier, error) {
 	// dedupe
 	var res []Verifier
 	verifierCheck := make(map[string]bool)
 	for _, verifier := range verifiers {
-		if _, ok := verifierCheck[verifier.Description]; !ok {
-			verifierCheck[verifier.Description] = true
+		if _, ok := verifierCheck[verifier.ID]; !ok {
+			verifierCheck[verifier.ID] = true
 			res = append(res, verifier)
 		}
 	}
@@ -44,15 +58,15 @@ func NewCredentialVerifier(verifiers []Verifier) (*CredentialVerifier, error) {
 }
 
 // VerifyCredential verifies a credential given a credential verifier
-func (cv *CredentialVerifier) VerifyCredential(cred credential.VerifiableCredential) error {
+func (cv *CredentialVerifier) VerifyCredential(cred credential.VerifiableCredential, opts ...VerificationOption) error {
 	ae := util.NewAppendError()
 	for _, verifier := range cv.verifiers {
-		if err := verifier.VerifyFunc(cred); err != nil {
-			ae.AppendString(fmt.Sprintf("[failed] for %s: %s", verifier.Description, err.Error()))
+		if err := verifier.VerifyFunc(cred, opts...); err != nil {
+			ae.AppendString(fmt.Sprintf("[validator: %s]: %s", verifier.ID, err.Error()))
 		}
 	}
 	if !ae.IsEmpty() {
-		return errors.Wrapf(ae.Error(), "credential verification failed with [%d]: %", ae.NumErrors())
+		return fmt.Errorf("credential verification failed with <%d> error, %s", ae.NumErrors(), ae.Error().Error())
 	}
 	return nil
 }
