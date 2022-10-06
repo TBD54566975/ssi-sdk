0be992d222cdd72109010bcc7e4da3c62ba3642a
Gabe <gcohen@squareup.com>
2022-10-05 19:48:29 -0700

    Dynamic Credential Verification (#217)
    * update simple wallet
    
    * verify temp
    
    * cred verification
    
    * pr comments
    
    * update wallet mutex

---
+++ credential/verification/verification.go
@@ -0,0 +1,72 @@
+package verification
+
+import (
+	"fmt"
+
+	"github.com/TBD54566975/ssi-sdk/credential"
+	"github.com/TBD54566975/ssi-sdk/util"
+	"github.com/pkg/errors"
+)
+
+type CredentialVerifier struct {
+	verifiers []Verifier
+}
+
+type Verifier struct {
+	ID         string
+	VerifyFunc Verify
+}
+
+type (
+	// OptionKey uniquely represents an option to be used in a verifier
+	OptionKey string
+)
+
+// VerificationOption represents a single option that may be required for a verifier
+type VerificationOption struct {
+	ID     OptionKey
+	Option interface{}
+}
+
+// GetVerificationOption returns a verification option given an ID
+func GetVerificationOption(opts []VerificationOption, id OptionKey) (interface{}, error) {
+	for _, opt := range opts {
+		if opt.ID == id {
+			return opt.Option, nil
+		}
+	}
+	return nil, errors.Errorf("option with id <%s> not found", id)
+}
+
+type Verify func(credential credential.VerifiableCredential, opts ...VerificationOption) error
+
+// NewCredentialVerifier creates a new credential verifier which executes in the order of the verifiers provided
+func NewCredentialVerifier(verifiers []Verifier) (*CredentialVerifier, error) {
+	// dedupe
+	var deduplicatedVerifiers []Verifier
+	verifierCheck := make(map[string]bool)
+	for _, verifier := range verifiers {
+		if _, ok := verifierCheck[verifier.ID]; !ok {
+			verifierCheck[verifier.ID] = true
+			deduplicatedVerifiers = append(deduplicatedVerifiers, verifier)
+		}
+	}
+	if len(deduplicatedVerifiers) == 0 {
+		return nil, errors.New("no verifiers provided")
+	}
+	return &CredentialVerifier{verifiers: deduplicatedVerifiers}, nil
+}
+
+// VerifyCredential verifies a credential given a credential verifier
+func (cv *CredentialVerifier) VerifyCredential(cred credential.VerifiableCredential, opts ...VerificationOption) error {
+	ae := util.NewAppendError()
+	for _, verifier := range cv.verifiers {
+		if err := verifier.VerifyFunc(cred, opts...); err != nil {
+			ae.AppendString(fmt.Sprintf("[validator: %s]: %s", verifier.ID, err.Error()))
+		}
+	}
+	if !ae.IsEmpty() {
+		return fmt.Errorf("credential verification failed with <%d> error, %s", ae.NumErrors(), ae.Error().Error())
+	}
+	return nil
+}
---
+++ credential/verification/verifiers.go
@@ -0,0 +1,94 @@
+package verification
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/TBD54566975/ssi-sdk/credential"
+	credschema "github.com/TBD54566975/ssi-sdk/credential/schema"
+	"github.com/pkg/errors"
+)
+
+const (
+	SchemaOption OptionKey = "schema"
+)
+
+var KnownVerifiers = []Verifier{
+	{
+		ID:         "Object Validation",
+		VerifyFunc: VerifyValidCredential,
+	},
+	{
+		ID:         "Expiry Check",
+		VerifyFunc: VerifyExpiry,
+	},
+	{
+		ID:         "VC JSON Schema",
+		VerifyFunc: VerifyJSONSchema,
+	},
+}
+
+// VerifyValidCredential verifies a credential's object model depending on the struct tags used on VerifiableCredential
+// TODO(gabe) add support for JSON schema validation of the VCDM after https://github.com/w3c/vc-data-model/issues/934
+func VerifyValidCredential(credential credential.VerifiableCredential, _ ...VerificationOption) error {
+	return credential.IsValid()
+}
+
+// VerifyExpiry verifies a credential's expiry date is not in the past. We assume the date is parseable as
+// an RFC3339 date time value.
+func VerifyExpiry(credential credential.VerifiableCredential, _ ...VerificationOption) error {
+	if credential.ExpirationDate == "" {
+		return nil
+	}
+	expiryTime, err := time.Parse(time.RFC3339, credential.ExpirationDate)
+	if err != nil {
+		return errors.Wrapf(err, "failed to parse expiry date: %s", credential.ExpirationDate)
+	}
+	if expiryTime.Before(time.Now()) {
+		return fmt.Errorf("credential has expired as of %s", expiryTime.String())
+	}
+	return nil
+}
+
+// WithSchema provides a schema as a verification option
+func WithSchema(schema string) VerificationOption {
+	return VerificationOption{
+		ID:     SchemaOption,
+		Option: schema,
+	}
+}
+
+// VerifyJSONSchema verifies a credential's data against a Verifiable Credential JSON Schema:
+// https://w3c-ccg.github.io/vc-json-schemas/v2/index.html#credential_schema_definition
+// There is a required single option which is a string JSON value representing the Credential Schema Object
+func VerifyJSONSchema(credential credential.VerifiableCredential, opts ...VerificationOption) error {
+	hasSchemaProperty := credential.CredentialSchema != nil
+	schema, err := GetVerificationOption(opts, SchemaOption)
+	if err != nil {
+		// if the credential does not have a schema property, we cannot perform this check
+		if !hasSchemaProperty {
+			return nil
+		}
+		return errors.Wrap(err, "cannot verify the credential against a JSON schema, no schema provided")
+	}
+	// if the credential does not have a schema property, we cannot perform this check
+	if !hasSchemaProperty {
+		return errors.New("credential does not have a credentialSchema property")
+	}
+	credSchema, err := optionToCredentialSchema(schema)
+	if err != nil {
+		return err
+	}
+	return credschema.IsCredentialValidForVCJSONSchema(credential, *credSchema)
+}
+
+func optionToCredentialSchema(maybeSchema interface{}) (*credschema.VCJSONSchema, error) {
+	schema, ok := maybeSchema.(string)
+	if !ok {
+		return nil, errors.New("the option provided must be a string value representing a Verifiable Credential JSON Schema")
+	}
+	if err := credschema.IsValidCredentialSchema(schema); err != nil {
+		return nil, errors.Wrap(err, "credential schema is invalid")
+	}
+	return credschema.StringToVCJSONCredentialSchema(schema)
+}
