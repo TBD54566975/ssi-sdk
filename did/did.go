560f7ccb3b938b5c6de960b068230fa90fc35493
Gabe <gcohen@squareup.com>
2022-10-06 18:03:36 -0700

    move did resolver to a new file, make method public (#218)
    * fix
    
    * update go version
    
    * update go version

--- did/did.go
+++ did/did.go
@@ -1,13 +1,5 @@
 package did
 
-import (
-	"fmt"
-	"strings"
-
-	"github.com/TBD54566975/ssi-sdk/util"
-	"github.com/pkg/errors"
-)
-
 type (
 	Method string
 )
@@ -19,6 +11,10 @@ const (
 	WebMethod  Method = "web"
 )
 
+func (m Method) String() string {
+	return string(m)
+}
+
 // DID represents functionality common to all DIDs
 type DID interface {
 	// IsValid checks if the DID is compliant with its methods definition
@@ -29,61 +25,4 @@ type DID interface {
 	Suffix() (string, error)
 	// Method provides the method for the DID
 	Method() Method
-}
-
-// ResolutionOptions https://www.w3.org/TR/did-spec-registries/#did-resolution-options
-type ResolutionOptions interface{}
-
-// Resolution provides an interface for resolving DIDs as per the spec https://www.w3.org/TR/did-core/#did-resolution
-type Resolution interface {
-	// Resolve Attempts to resolve a DID for a given method
-	Resolve(did string, opts ResolutionOptions) (*DIDResolutionResult, error)
-	// Method provides the method for the given resolution implementation
-	Method() Method
-}
-
-// Resolver resolves a DID. The current implementation ssk-sdk does not have a universal resolver:
-// https://github.com/decentralized-identity/universal-resolver
-// In its place, this method attempts to resolve DID methods that can be resolved without relying on additional services.
-type Resolver struct {
-	resolvers map[Method]Resolution
-	methods   []Method
-}
-
-func NewResolver(resolvers ...Resolution) (*Resolver, error) {
-	r := make(map[Method]Resolution)
-	var methods []Method
-	for _, resolver := range resolvers {
-		method := resolver.Method()
-		if _, ok := r[method]; ok {
-			return nil, util.LoggingNewError(fmt.Sprintf("duplicate resolver for method: %s", method))
-		}
-		r[method] = resolver
-		methods = append(methods, method)
-	}
-	return &Resolver{resolvers: r, methods: methods}, nil
-}
-
-// Resolve attempts to resolve a DID for a given method
-func (dr Resolver) Resolve(did string, opts ...ResolutionOptions) (*DIDResolutionResult, error) {
-	method, err := getMethodForDID(did)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get method for DID before resolving")
-	}
-	if resolver, ok := dr.resolvers[method]; ok {
-		return resolver.Resolve(did, opts)
-	}
-	return nil, util.LoggingNewError(fmt.Sprintf("unsupported method: %s", method))
-}
-
-func (dr Resolver) SupportedMethods() []Method {
-	return dr.methods
-}
-
-func getMethodForDID(did string) (Method, error) {
-	split := strings.Split(did, ":")
-	if len(split) < 3 {
-		return "", fmt.Errorf("not a valid did: %s", did)
-	}
-	return Method(split[1]), nil
 }
