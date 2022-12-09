4008c6d0361ad29a3400922193b6b3eb288b4e65
Mic Neale <michael.neale@gmail.com>
2022-12-09 17:34:10 +1100

    initial wasm

---
+++ wasm/go.mod
@@ -0,0 +1,36 @@
+module sample-app
+
+go 1.19
+
+require github.com/TBD54566975/ssi-sdk v0.0.0-20220624004206-a80f5b59b034
+
+replace github.com/TBD54566975/ssi-sdk => ../
+
+require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.11.1 // indirect
+	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.0 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/iter v1.0.1 // indirect
+	github.com/lestrrat-go/jwx v1.2.25 // indirect
+	github.com/lestrrat-go/option v1.0.0 // indirect
+	github.com/mr-tron/base58 v1.1.3 // indirect
+	github.com/multiformats/go-base32 v0.0.3 // indirect
+	github.com/multiformats/go-base36 v0.1.0 // indirect
+	github.com/multiformats/go-multibase v0.1.1 // indirect
+	github.com/multiformats/go-multicodec v0.7.0 // indirect
+	github.com/multiformats/go-varint v0.0.7 // indirect
+	github.com/piprate/json-gold v0.5.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/crypto v0.0.0-20221005025214-4161e89ecf1b // indirect
+	golang.org/x/sys v0.0.0-20221006211917-84dc82d7e875 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ wasm/main.go
@@ -0,0 +1,62 @@
+//go:build js && wasm
+
+package main
+
+import (
+	"crypto/ed25519"
+	"encoding/base64"
+	"encoding/json"
+	"syscall/js"
+
+	"github.com/TBD54566975/ssi-sdk/crypto"
+	"github.com/TBD54566975/ssi-sdk/did"
+)
+
+/*
+ * This is the glue to bind the functions into javascript so they can be called
+ */
+func main() {
+	done := make(chan struct{})
+
+	// Bind the functions to javascript
+	js.Global().Set("sayHello", js.FuncOf(sayHello))
+	js.Global().Set("generateKey", js.FuncOf(generateKey))
+	js.Global().Set("makeDid", js.FuncOf(makeDid))
+
+	<-done
+}
+
+// 1. Simplest function - note we wrap things with js.ValueOf (if a primitive you don't technically need to)
+func sayHello(_ js.Value, args []js.Value) interface{} {
+	return js.ValueOf("Hello, Wasm World!")
+}
+
+// 2. Calling a ssi-sdk function directly - but returning a plain old string
+func generateKey(_ js.Value, args []js.Value) interface{} {
+
+	keyType := args[0].String()
+	for _, k := range crypto.GetSupportedKeyTypes() {
+		if string(k) == keyType {
+			publicKey, _, _ := crypto.GenerateKeyByKeyType(k)
+			pubKeyBytes, _ := crypto.PubKeyToBytes(publicKey)
+			return js.ValueOf(base64.StdEncoding.EncodeToString(pubKeyBytes))
+		}
+	}
+
+	return js.ValueOf("Unknown key type")
+}
+
+// 3. Returning a richer object, converting to json and then unmarshalling to make it a js object
+func makeDid(_ js.Value, args []js.Value) interface{} {
+
+	pubKey, _, _ := crypto.GenerateKeyByKeyType(crypto.Ed25519)
+	didKey, _ := did.CreateDIDKey(crypto.Ed25519, pubKey.(ed25519.PublicKey))
+	result, _ := didKey.Expand()
+
+	// unmarshall into json bytes, then back into a simple struct for converting to js
+	resultBytes, _ := json.Marshal(result)
+	var resultObj map[string]interface{}
+	json.Unmarshal(resultBytes, &resultObj)
+	return js.ValueOf(resultObj)
+
+}
