7eeb7b3128dc6dabecf813a983740f403a2a60c4
gabe <gcohen@squareup.com>
2023-02-20 13:38:44 -0800

    problem with bls verify

--- cryptosuite/bbsplussignatureproofsuite_test.go
+++ cryptosuite/bbsplussignatureproofsuite_test.go
@@ -24,45 +24,80 @@ var (
 )
 
 func TestBBSPlusSignatureProofSuite(t *testing.T) {
-	base58PubKey := "nEP2DEdbRaQ2r5Azeatui9MG6cj7JUHa8GD7khub4egHJREEuvj4Y8YG8w51LnhPEXxVV1ka93HpSLkVzeQuuPE1mH9oCMrqoHXAKGBsuDT1yJvj9cKgxxLCXiRRirCycki"
-	pubKeyBytes, err := base58.Decode(base58PubKey)
-	assert.NoError(t, err)
+	t.Run("generate our own credential and frame", func(tt *testing.T) {
+		// generate a test credential to selectively disclosure just the issuer
+		suite := GetBBSPlusSignatureSuite()
+		testCred := TestCredential{
+			Context: []any{"https://www.w3.org/2018/credentials/v1",
+				"https://w3id.org/security/bbs/v1"},
+			Type:         []string{"VerifiableCredential"},
+			Issuer:       "did:example:123",
+			IssuanceDate: "2021-01-01T19:23:24Z",
+			CredentialSubject: map[string]any{
+				"id": "did:example:abcd",
+			},
+		}
+		key, err := GenerateBLSKey2020()
+		assert.NoError(t, err)
+		privKey, err := key.GetPrivateKey()
+		assert.NoError(t, err)
+		signer, err := NewBBSPlusSigner("test-key-1", privKey, Authentication)
+		assert.NoError(t, err)
+		err = suite.Sign(signer, &testCred)
+		assert.NoError(t, err)
 
-	case16VC, err := getTestVector(TestVector1)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, case16VC)
+		proofSuite := GetBBSPlusSignatureProofSuite()
+		revealDoc := map[string]any{
+			"@context": []any{"https://www.w3.org/2018/credentials/v1", "https://w3id.org/security/bbs/v1"},
+			"type":     "VerifiableCredential",
+			"issuer":   map[string]any{},
+		}
+		selectiveDisclosure, err := proofSuite.SelectivelyDisclose(*signer, &testCred, revealDoc)
+		assert.NoError(tt, err)
+		assert.NotEmpty(tt, selectiveDisclosure)
+	})
 
-	var cred TestCredential
-	err = json.Unmarshal([]byte(case16VC), &cred)
-	assert.NoError(t, err)
+	t.Run("known test vector", func(tt *testing.T) {
+		base58PubKey := "nEP2DEdbRaQ2r5Azeatui9MG6cj7JUHa8GD7khub4egHJREEuvj4Y8YG8w51LnhPEXxVV1ka93HpSLkVzeQuuPE1mH9oCMrqoHXAKGBsuDT1yJvj9cKgxxLCXiRRirCycki"
+		pubKeyBytes, err := base58.Decode(base58PubKey)
+		assert.NoError(tt, err)
 
-	pubKey, err := bbsg2.UnmarshalPublicKey(pubKeyBytes)
-	assert.NoError(t, err)
-	signer := BBSPlusSigner{
-		BBSPlusSigner: crypto.BBSPlusSigner{
-			PublicKey: pubKey,
-		},
-	}
-	assert.NotEmpty(t, signer)
+		case16VC, err := getTestVector(TestVector1)
+		assert.NoError(tt, err)
+		assert.NotEmpty(tt, case16VC)
 
-	// First verify the credential as is
-	suite := GetBBSPlusSignatureSuite()
-	err = suite.Verify(&signer, &cred)
-	assert.NoError(t, err)
+		var cred TestCredential
+		err = json.Unmarshal([]byte(case16VC), &cred)
+		assert.NoError(tt, err)
 
-	// Test selective disclosure
+		pubKey, err := bbsg2.UnmarshalPublicKey(pubKeyBytes)
+		assert.NoError(tt, err)
+		signer := BBSPlusSigner{
+			BBSPlusSigner: crypto.BBSPlusSigner{
+				PublicKey: pubKey,
+			},
+		}
+		assert.NotEmpty(tt, signer)
 
-	// case16RevealDoc, err := getTestVector(TestVector1Reveal)
-	// assert.NoError(t, err)
-	// assert.NotEmpty(t, case16RevealDoc)
-	//
-	// var revealDoc map[string]any
-	// err = json.Unmarshal([]byte(case16RevealDoc), &revealDoc)
-	// assert.NoError(t, err)
-	//
-	// selectiveDisclosure, err := suite.SelectivelyDisclose(signer, &cred, revealDoc)
-	// assert.NoError(t, err)
-	// assert.NotEmpty(t, selectiveDisclosure)
+		// First verify the credential as is
+		suite := GetBBSPlusSignatureSuite()
+		err = suite.Verify(&signer, &cred)
+		assert.NoError(tt, err)
+
+		// Test selective disclosure
+
+		// case16RevealDoc, err := getTestVector(TestVector1Reveal)
+		// assert.NoError(tt, err)
+		// assert.NotEmpty(tt, case16RevealDoc)
+		//
+		// var revealDoc map[string]any
+		// err = json.Unmarshal([]byte(case16RevealDoc), &revealDoc)
+		// assert.NoError(tt, err)
+		//
+		// selectiveDisclosure, err := suite.SelectivelyDisclose(signer, &cred, revealDoc)
+		// assert.NoError(tt, err)
+		// assert.NotEmpty(tt, selectiveDisclosure)
+	})
 }
 
 func TestRoundTripTestVector(t *testing.T) {
