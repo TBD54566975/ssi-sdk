58ab97236a1d25ea1a812449bac000328b7f8fdd
gabe <gcohen@squareup.com>
2023-03-17 16:51:43 -0700

    deactivate request

--- did/ion/request.go
+++ did/ion/request.go
@@ -2,6 +2,7 @@ package ion
 
 import (
 	"github.com/TBD54566975/ssi-sdk/crypto"
+	"github.com/goccy/go-json"
 )
 
 // NewCreateRequest creates a new create request https://identity.foundation/sidetree/spec/#create
@@ -46,5 +47,45 @@ func NewCreateRequest(recoveryKey, updateKey crypto.PublicKeyJWK, document Docum
 		Type:       Create,
 		SuffixData: suffixData,
 		Delta:      delta,
+	}, nil
+}
+
+// NewDeactivateRequest creates a new deactivate request https://identity.foundation/sidetree/spec/#deactivate
+func NewDeactivateRequest(didSuffix string, recoveryKey crypto.PublicKeyJWK, signer crypto.JWTSigner) (*DeactivateRequest, error) {
+	// prepare reveal value
+	recoveryKeyCanonical, err := CanonicalizeAny(recoveryKey)
+	if err != nil {
+		return nil, err
+	}
+	revealValue, err := HashEncode(recoveryKeyCanonical)
+	if err != nil {
+		return nil, err
+	}
+
+	// prepare signed data
+	toBeSigned := struct {
+		DIDSuffix   string              `json:"didSuffix"`
+		RecoveryKey crypto.PublicKeyJWK `json:"recoveryKey"`
+	}{
+		DIDSuffix:   didSuffix,
+		RecoveryKey: recoveryKey,
+	}
+	toBeSignedBytes, err := json.Marshal(toBeSigned)
+	if err != nil {
+		return nil, err
+	}
+	var toBeSignedJSON map[string]any
+	if err = json.Unmarshal(toBeSignedBytes, &toBeSignedJSON); err != nil {
+		return nil, err
+	}
+	signedJWT, err := signer.SignJWT(toBeSignedJSON)
+	if err != nil {
+		return nil, err
+	}
+	return &DeactivateRequest{
+		Type:        Deactivate,
+		DIDSuffix:   didSuffix,
+		RevealValue: revealValue,
+		SignedData:  string(signedJWT),
 	}, nil
 }
