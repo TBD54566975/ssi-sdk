c7c66efd3b1aef891f853d5d390ee06e3a1360de
gabe <gcohen@squareup.com>
2023-03-20 12:55:51 -0700

    result test vectors passing

--- did/ion/request_test.go
+++ did/ion/request_test.go
@@ -110,12 +110,88 @@ func TestUpdateRequest(t *testing.T) {
 	assert.Len(t, updateRequest.Delta.Patches, 4)
 }
 
-func TestRecoverRequest(_ *testing.T) {
+func TestRecoverRequest(t *testing.T) {
+	publicKeyJSON, err := getTestData("publickeymodel1.json")
+	assert.NoError(t, err)
+	var publicKey PublicKey
+	err = json.Unmarshal([]byte(publicKeyJSON), &publicKey)
+	assert.NoError(t, err)
 
+	serviceJSON, err := getTestData("service1.json")
+	assert.NoError(t, err)
+	var service Service
+	err = json.Unmarshal([]byte(serviceJSON), &service)
+	assert.NoError(t, err)
+
+	document := Document{PublicKeys: []PublicKey{publicKey}, Services: []Service{service}}
+
+	recoveryKeyJSON, err := getTestData("jwkes256k1public.json")
+	assert.NoError(t, err)
+	var recoveryKey crypto.PublicKeyJWK
+	err = json.Unmarshal([]byte(recoveryKeyJSON), &recoveryKey)
+	assert.NoError(t, err)
+
+	recoveryPrivateKeyJSON, err := getTestData("jwkes256k1private.json")
+	assert.NoError(t, err)
+	var recoveryPrivateKey crypto.PrivateKeyJWK
+	err = json.Unmarshal([]byte(recoveryPrivateKeyJSON), &recoveryPrivateKey)
+	assert.NoError(t, err)
+
+	nextRecoveryKeyJSON, err := getTestData("jwkes256k2public.json")
+	assert.NoError(t, err)
+	var nextRecoveryKey crypto.PublicKeyJWK
+	err = json.Unmarshal([]byte(nextRecoveryKeyJSON), &nextRecoveryKey)
+	assert.NoError(t, err)
+
+	nextUpdateKeyJSON, err := getTestData("jwkes256k3public.json")
+	assert.NoError(t, err)
+	var nextUpdateKey crypto.PublicKeyJWK
+	err = json.Unmarshal([]byte(nextUpdateKeyJSON), &nextUpdateKey)
+	assert.NoError(t, err)
+
+	signer, err := NewBTCSignerVerifier(recoveryPrivateKey)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, signer)
+
+	didSuffix := "EiDyOQbbZAa3aiRzeCkV7LOx3SERjjH93EXoIM3UoN4oWg"
+	recoverRequest, err := NewRecoverRequest(didSuffix, recoveryKey, nextRecoveryKey, nextUpdateKey, document, *signer)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, recoverRequest)
+
+	assert.Equal(t, didSuffix, recoverRequest.DIDSuffix)
+	assert.Equal(t, "EiAJ-97Is59is6FKAProwDo870nmwCeP8n5nRRFwPpUZVQ", recoverRequest.RevealValue)
+	assert.Equal(t, Recover, recoverRequest.Type)
+	assert.Equal(t, recoverRequest.SignedData, "eyJhbGciOiJFUzI1NksifQ.eyJyZWNvdmVyeUNvbW1pdG1lbnQiOiJFaURLSWt3cU82OUlQRzNwT2xIa2RiODZuWXQwYU54U0hadTJyLWJoRXpuamRBIiwicmVjb3ZlcnlLZXkiOnsia3R5IjoiRUMiLCJjcnYiOiJzZWNwMjU2azEiLCJ4IjoibklxbFJDeDBleUJTWGNRbnFEcFJlU3Y0enVXaHdDUldzc29jOUxfbmo2QSIsInkiOiJpRzI5Vks2bDJVNXNLQlpVU0plUHZ5RnVzWGdTbEsyZERGbFdhQ004RjdrIn0sImRlbHRhSGFzaCI6IkVpQm9HNlFtamlTSm5ON2phaldnaV9vZDhjR3dYSm9Nc2RlWGlWWTc3NXZ2SkEifQ.58n6Fel9DmRAXxwcJMUwYaUhmj5kigKMNrGjr7eJaJcjOmjvwlKLSjiovWiYrb9yjkfMAjpgbAdU_2EDI1_lZw")
+	assert.Equal(t, "EiBJGXo0XUiqZQy0r-fQUHKS3RRVXw5nwUpqGVXEGuTs-g", recoverRequest.Delta.UpdateCommitment)
+	assert.Len(t, recoverRequest.Delta.Patches, 1)
 }
 
-func TestDeactivateRequest(_ *testing.T) {
+func TestDeactivateRequest(t *testing.T) {
+	recoveryKeyJSON, err := getTestData("jwkes256k1public.json")
+	assert.NoError(t, err)
+	var recoveryKey crypto.PublicKeyJWK
+	err = json.Unmarshal([]byte(recoveryKeyJSON), &recoveryKey)
+	assert.NoError(t, err)
 
+	recoveryPrivateKeyJSON, err := getTestData("jwkes256k1private.json")
+	assert.NoError(t, err)
+	var recoveryPrivateKey crypto.PrivateKeyJWK
+	err = json.Unmarshal([]byte(recoveryPrivateKeyJSON), &recoveryPrivateKey)
+	assert.NoError(t, err)
+
+	signer, err := NewBTCSignerVerifier(recoveryPrivateKey)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, signer)
+
+	didSuffix := "EiDyOQbbZAa3aiRzeCkV7LOx3SERjjH93EXoIM3UoN4oWg"
+	deactivateRequest, err := NewDeactivateRequest(didSuffix, recoveryKey, *signer)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, deactivateRequest)
+
+	assert.Equal(t, didSuffix, deactivateRequest.DIDSuffix)
+	assert.Equal(t, Deactivate, deactivateRequest.Type)
+	assert.Equal(t, deactivateRequest.RevealValue, "EiAJ-97Is59is6FKAProwDo870nmwCeP8n5nRRFwPpUZVQ")
+	assert.Equal(t, deactivateRequest.SignedData, "eyJhbGciOiJFUzI1NksifQ.eyJkaWRTdWZmaXgiOiJFaUR5T1FiYlpBYTNhaVJ6ZUNrVjdMT3gzU0VSampIOTNFWG9JTTNVb040b1dnIiwicmVjb3ZlcnlLZXkiOnsia3R5IjoiRUMiLCJjcnYiOiJzZWNwMjU2azEiLCJ4IjoibklxbFJDeDBleUJTWGNRbnFEcFJlU3Y0enVXaHdDUldzc29jOUxfbmo2QSIsInkiOiJpRzI5Vks2bDJVNXNLQlpVU0plUHZ5RnVzWGdTbEsyZERGbFdhQ004RjdrIn19.uLgnDBmmFzST4VTmdJcmFKVicF0kQaBqEnRQLbqJydgIg_2oreihCA5sBBIUBlSXwvnA9xdK97ksJGmPQ7asPQ")
 }
 
 func getTestData(fileName string) (string, error) {
