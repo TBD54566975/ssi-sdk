1234e628c5241e7d113e16f6d87fa03c491741f8
Gabe <7622243+decentralgabe@users.noreply.github.com>
2023-03-22 13:15:49 -0700

    make target optional (#316)
    * make target optional
    
    * merge

--- credential/exchange/request.go
+++ credential/exchange/request.go
@@ -21,18 +21,54 @@ const (
 	// JWT key values
 
 	PresentationDefinitionKey string = "presentation_definition"
+
+	// Presentation Request Option types
+
+	TargetOption PresentationRequestOptionType = "target"
 )
+
+type PresentationRequestOptionType string
+
+type PresentationRequestOption struct {
+	Type  PresentationRequestOptionType
+	Value any
+}
 
 // BuildPresentationRequest https://identity.foundation/presentation-exchange/#presentation-request
 // used for transmitting a Presentation Definition from a holder to a verifier. Target is who the request is intended for.
 // TODO(gabe) expand to other presentation types and signers https://github.com/TBD54566975/ssi-sdk/issues/57
-func BuildPresentationRequest(signer crypto.JWTSigner, pt PresentationRequestType, def PresentationDefinition, target string) ([]byte, error) {
+func BuildPresentationRequest(signer any, pt PresentationRequestType, def PresentationDefinition, opts ...PresentationRequestOption) ([]byte, error) {
+	if signer == nil {
+		return nil, fmt.Errorf("cannot build presentation request with nil signer")
+	}
+
+	// process options
+	if len(opts) > 1 {
+		return nil, fmt.Errorf("only one option supported")
+	}
+	var target string
+	if len(opts) == 1 {
+		opt := opts[0]
+		if opt.Type != TargetOption {
+			return nil, fmt.Errorf("unsupported option type: %s", opt.Type)
+		}
+		var ok bool
+		target, ok = opt.Value.(string)
+		if !ok {
+			return nil, fmt.Errorf("target option value must be a string")
+		}
+	}
+
 	if !IsSupportedPresentationRequestType(pt) {
 		return nil, fmt.Errorf("unsupported presentation request type: %s", pt)
 	}
 	switch pt {
 	case JWTRequest:
-		return BuildJWTPresentationRequest(signer, def, target)
+		jwtSigner, ok := signer.(crypto.JWTSigner)
+		if !ok {
+			return nil, errors.New("signer is not a JWTSigner")
+		}
+		return BuildJWTPresentationRequest(jwtSigner, def, target)
 	default:
 		return nil, fmt.Errorf("presentation request type <%s> is not implemented", pt)
 	}
@@ -45,6 +81,9 @@ func BuildJWTPresentationRequest(signer crypto.JWTSigner, def PresentationDefini
 		jwt.IssuerKey:             signer.KeyID(),
 		jwt.AudienceKey:           target,
 		PresentationDefinitionKey: def,
+	}
+	if target != "" {
+		jwtValues[jwt.AudienceKey] = target
 	}
 	return signer.SignWithDefaults(jwtValues)
 }
