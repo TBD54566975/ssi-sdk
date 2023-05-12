87cc63e545d4d6452b3ebb528b3a18309a907ffc
Gabe <7622243+decentralgabe@users.noreply.github.com>
2023-05-12 11:22:44 -0500

    Update did/key/key.go
    Co-authored-by: Andres Uribe <auribe@tbd.email>

--- did/key/key.go
+++ did/key/key.go
@@ -307,7 +307,7 @@ func generateKeyAgreementVerificationMethod(vm did.VerificationMethod) (*did.Ver
 		}
 		id, x25519Key, err := x25519KeyAndID(vm.Controller, ed25519PubKey.(ed25519.PublicKey))
 		if err != nil {
-			return nil, nil, errors.Wrap(err, "could not generate x25519 key and id")
+			return nil, nil, errors.Wrap(err, "generating x25519 key and id")
 		}
 		verificationMethod, vmErr = did.ConstructMultibaseVerificationMethod(id, vm.Controller, x25519Key, cryptosuite.X25519KeyAgreementKey2020)
 		verificationMethodSet = []did.VerificationMethodSet{id}
