07707db2c4040bfad689896025b2262fe25214ae
Vlad Timofeev <11474041+vlad-timofeev@users.noreply.github.com>
2023-06-01 11:42:59 -0400

    Refactor IsValid() in did/web/web.go (#399)
    * refactor web IsValid
    
    * update error handling and verification
    
    ---------
    
    Co-authored-by: Gabe <7622243+decentralgabe@users.noreply.github.com>

--- did/web/resolver.go
+++ did/web/resolver.go
@@ -21,12 +21,12 @@ func (Resolver) Methods() []did.Method {
 
 // Resolve fetches and returns the Document from the expected URL
 // specification: https://w3c-ccg.github.io/did-method-web/#read-resolve
-func (Resolver) Resolve(_ context.Context, id string, _ ...resolution.Option) (*resolution.Result, error) {
+func (Resolver) Resolve(ctx context.Context, id string, _ ...resolution.Option) (*resolution.Result, error) {
 	if !strings.HasPrefix(id, Prefix) {
 		return nil, fmt.Errorf("not a did:web DID: %s", id)
 	}
 	didWeb := DIDWeb(id)
-	doc, err := didWeb.Resolve()
+	doc, err := didWeb.Resolve(ctx)
 	if err != nil {
 		return nil, errors.Wrapf(err, "cresolving did:web DID: %s", id)
 	}
