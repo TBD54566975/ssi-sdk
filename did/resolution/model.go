992b1fb85a80d7ef243d0fc49eabd83510e16c31
Andres Uribe Gonzalez <auribe@squareup.com>
2023-07-27 13:20:03 -0400

    Fixed resolution problems.

--- did/resolution/model.go
+++ did/resolution/model.go
@@ -10,7 +10,7 @@ import (
 // Result encapsulates the tuple of a DID resolution https://www.w3.org/TR/did-core/#did-resolution
 type Result struct {
 	Context          string `json:"@context,omitempty"`
-	Metadata         `json:"didResolutionMetadata,omitempty"`
+	*Metadata        `json:"didResolutionMetadata,omitempty"`
 	did.Document     `json:"didDocument,omitempty"`
 	DocumentMetadata `json:"didDocumentMetadata,omitempty"`
 }
@@ -22,16 +22,23 @@ func (r *Result) IsEmpty() bool {
 	return reflect.DeepEqual(r, Result{})
 }
 
+type Method struct {
+	Published          bool   `json:"published"`
+	RecoveryCommitment string `json:"recoveryCommitment,omitempty"`
+	UpdateCommitment   string `json:"updateCommitment,omitempty"`
+}
+
 // DocumentMetadata https://www.w3.org/TR/did-core/#did-document-metadata
 type DocumentMetadata struct {
-	Created       string `json:"created,omitempty" validate:"omitempty,datetime=2006-01-02T15:04:05Z"`
-	Updated       string `json:"updated,omitempty" validate:"omitempty,datetime=2006-01-02T15:04:05Z"`
-	Deactivated   bool   `json:"deactivated,omitempty"`
-	NextUpdate    string `json:"nextUpdate,omitempty"`
-	VersionID     string `json:"versionId,omitempty"`
-	NextVersionID string `json:"nextVersionId,omitempty"`
-	EquivalentID  string `json:"equivalentId,omitempty"`
-	CanonicalID   string `json:"canonicalId,omitempty"`
+	Created       string   `json:"created,omitempty" validate:"omitempty,datetime=2006-01-02T15:04:05Z"`
+	Updated       string   `json:"updated,omitempty" validate:"omitempty,datetime=2006-01-02T15:04:05Z"`
+	Deactivated   bool     `json:"deactivated,omitempty"`
+	NextUpdate    string   `json:"nextUpdate,omitempty"`
+	VersionID     string   `json:"versionId,omitempty"`
+	NextVersionID string   `json:"nextVersionId,omitempty"`
+	EquivalentID  []string `json:"equivalentId,omitempty"`
+	CanonicalID   string   `json:"canonicalId,omitempty"`
+	Method        Method   `json:"method,omitempty"`
 }
 
 func (s *DocumentMetadata) IsValid() bool {
@@ -48,6 +55,6 @@ type Error struct {
 
 // Metadata https://www.w3.org/TR/did-core/#did-resolution-metadata
 type Metadata struct {
-	ContentType string
-	Error       *Error
+	ContentType string `json:"contentType,omitempty"`
+	Error       *Error `json:"error,omitempty"`
 }
