c03bb12eb58360f7919406886849bb100c62996b
gabe <gcohen@squareup.com>
2024-04-09 23:00:34 -0400

    context loggers

--- util/errors.go
+++ util/errors.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
@@ -23,6 +24,12 @@ func LoggingError(err error) error {
 	return err
 }
 
+// LoggingCtxError is a utility to combine logging an error, and returning and error
+func LoggingCtxError(ctx context.Context, err error) error {
+	logrus.WithContext(ctx).WithError(err).Error()
+	return err
+}
+
 // LoggingNewError is a utility to create an error from a message, log it, and return it as an error
 func LoggingNewError(msg string) error {
 	err := errors.New(msg)
@@ -30,9 +37,21 @@ func LoggingNewError(msg string) error {
 	return err
 }
 
+// LoggingCtxNewError is a utility to create an error from a message, log it, and return it as an error
+func LoggingCtxNewError(ctx context.Context, msg string) error {
+	err := errors.New(msg)
+	logrus.WithContext(ctx).WithError(err).Error()
+	return err
+}
+
 // LoggingNewErrorf is a utility to create an error from a formatted message, log it, and return it as an error
 func LoggingNewErrorf(msg string, args ...any) error {
 	return LoggingNewError(fmt.Sprintf(msg, args...))
+}
+
+// LoggingCtxNewErrorf is a utility to create an error from a formatted message, log it, and return it as an error
+func LoggingCtxNewErrorf(ctx context.Context, msg string, args ...any) error {
+	return LoggingCtxNewError(ctx, fmt.Sprintf(msg, args...))
 }
 
 // LoggingErrorMsg is a utility to combine logging an error, and returning and error with a message
@@ -44,9 +63,23 @@ func LoggingErrorMsg(err error, msg string) error {
 	return errors.Wrap(err, msg)
 }
 
+// LoggingCtxErrorMsg is a utility to combine logging an error, and returning and error with a message
+func LoggingCtxErrorMsg(ctx context.Context, err error, msg string) error {
+	logrus.WithContext(ctx).WithError(err).Error(SanitizeLog(msg))
+	if err == nil {
+		return errors.New(msg)
+	}
+	return errors.Wrap(err, msg)
+}
+
 // LoggingErrorMsgf is a utility to combine logging an error, and returning and error with a formatted message
 func LoggingErrorMsgf(err error, msg string, args ...any) error {
 	return LoggingErrorMsg(err, fmt.Sprintf(msg, args...))
+}
+
+// LoggingCtxErrorMsgf is a utility to combine logging an error, and returning and error with a formatted message
+func LoggingCtxErrorMsgf(ctx context.Context, err error, msg string, args ...any) error {
+	return LoggingCtxErrorMsg(ctx, err, fmt.Sprintf(msg, args...))
 }
 
 // SanitizeLog prevents certain classes of injection attacks before logging
